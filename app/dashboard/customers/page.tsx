dad0b7331bf3c8d50a1044eef818ef249ad27f34
Antony Carrizo <antonycarrizo96@gmail.com>
2024-01-23 22:00:16 -0400

    [ADD]: add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,22 @@
 import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-    return <p>Customers</p>
+export  default async function Page(
+  {
+    searchParams,
+} : {   
+    searchParams?: {
+        query?: string;
+        page?: string;
+    } 
+}
+) {
+  const customers = await fetchFilteredCustomers(searchParams?.query || '');
+
+  return <CustomersTable customers={customers} />
 }
